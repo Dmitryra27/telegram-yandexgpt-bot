6294887c79e24098bfac1cd7ab287803a4f253dc
Dmitryra27 <dmitryra27@gmail.com>
2025-07-11 22:52:28 +0300

    First commit

---
+++ ai/yandex_gpt.py
@@ -0,0 +1,40 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FOLDER_ID = os.getenv("YANDEX_FOLDER_ID")
+API_KEY = os.getenv("YANDEX_API_KEY")
+
+def generate_text(prompt: str) -> str:
+    headers = {
+        "Authorization": f"Api-Key {API_KEY}",
+        "Content-Type": "application/json",
+    }
+
+    data = {
+        "modelUri": f"gpt://{FOLDER_ID}/yandexgpt-lite",
+        "completionOptions": {
+            "stream": False,
+            "temperature": 0.6,
+            "maxTokenCount": 1000
+        },
+        "messages": [
+            {
+                "role": "user",
+                "text": prompt
+            }
+        ]
+    }
+
+    response = requests.post(
+        "https://llm.api.cloud.yandex.net/foundationModels/v1/completion ",
+        headers=headers,
+        json=data
+    )
+
+    if response.status_code == 200:
+        return response.json()['result']['alternatives'][0]['message']['text']
+    else:
+        return f"Ошибка YandexGPT: {response.status_code}, {response.text}"
---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from ai.yandex_gpt import generate_text
+
+app = Flask(__name__)
+
+@app.route('/api/generate')
+def generate():
+    prompt = request.args.get('prompt')
+    if not prompt:
+        return "Нет запроса", 400
+    return generate_text(prompt)
+
+@app.route('/')
+def home():
+    return open('webapp/index.html').read()
+
+if __name__ == '__main__':
+    app.run()
