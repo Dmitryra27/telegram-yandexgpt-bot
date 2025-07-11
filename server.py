e5b97f15a464ad629b9b1c61c87369c0ff1e0177
Dmitryra27 <dmitryra27@gmail.com>
2025-07-12 00:32:11 +0300

    Fifth commit - устранение недочетов

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from ai.yandex_gpt import generate_text
 
 app = Flask(__name__)
@@ -9,13 +9,17 @@ def generate():
     prompt = request.args.get('prompt')
     if not prompt:
         return "Нет запроса", 400
-    return generate_text(prompt)
+    response = generate_text(prompt)
+    return Response(response, content_type='text/html')
+
 
 
 @app.route('/')
 def home():
-    return open('webapp/index.html').read()
+    html = open('index.html', encoding='utf-8').read()
+    return Response(html, content_type='text/html; charset=utf-8')
 
 
 if __name__ == '__main__':
-    app.run()
+    #app.run()# локально
+    app.run(host='0.0.0.0', port=8080)# на хостинге
