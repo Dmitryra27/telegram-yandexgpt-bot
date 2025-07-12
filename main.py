d45890108633850736f6d16fb447af64d3592776
Dmitryra27 <dmitryra27@gmail.com>
2025-07-12 18:35:37 +0300

    14 commit

--- main.py
+++ main.py
@@ -1,33 +1,13 @@
-import asyncio
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message, WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
-from ai.yandex_gpt import generate_text
+from flask import Flask, jsonify
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+app = Flask(__name__)
 
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-WEBAPP_URL = os.getenv("harmonious-charisma-production.up.railway.app", "harmonious-charisma-production.up.railway.app")
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
+@app.route('/')
+def index():
+    return jsonify({"Welcome to your Flask app !!"})
 
-@dp.message(F.text == "/start")
-async def cmd_start(message: Message):
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[[
-        InlineKeyboardButton(text="💬 Открыть AI-чат", web_app=WebAppInfo(url=WEBAPP_URL))
-    ]])
-    await message.answer("Привет! Нажми кнопку ниже, чтобы открыть веб-интерфейс.", reply_markup=keyboard)
 
-@dp.message()
-async def handle_message(message: Message):
-    user_text = message.text
-    reply = generate_text(user_text)
-    await message.answer(reply)
-
-async def main():
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
---
+++ main2.py
@@ -0,0 +1,33 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message, WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
+from ai.yandex_gpt import generate_text
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+WEBAPP_URL = os.getenv("harmonious-charisma-production.up.railway.app", "harmonious-charisma-production.up.railway.app")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(F.text == "/start")
+async def cmd_start(message: Message):
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[
+        InlineKeyboardButton(text="💬 Открыть AI-чат", web_app=WebAppInfo(url=WEBAPP_URL))
+    ]])
+    await message.answer("Привет! Нажми кнопку ниже, чтобы открыть веб-интерфейс.", reply_markup=keyboard)
+
+@dp.message()
+async def handle_message(message: Message):
+    user_text = message.text
+    reply = generate_text(user_text)
+    await message.answer(reply)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
